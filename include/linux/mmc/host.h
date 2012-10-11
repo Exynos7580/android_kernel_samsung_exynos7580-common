5ad758c26bd1a133245724debd8bf166d5fe1b17
Sujit Reddy Thumma <sthumma@codeaurora.org>
2012-10-11 17:17:03 +0530

    mmc: core: Add load based clock scaling support
    The SD3.0/eMMC4.5/SDIO3.0 cards can support clock rates upto
    200MHz (SDR104 or HS200 bus speed modes). For some workloads
    like video playback it isn't necessary for these cards to run
    at such high speed. Running at lower frequency, say 50MHz, in
    such cases can still meet the deadlines for data transfers.
    Scaling down the clock frequency dynamically has huge power
    savings not only because the bus is running at lower frequency
    but also has an advantage of scaling down the system core
    voltage, if supported.
    
    Provide an ondemand clock scaling support similar to cpufreq
    ondemand governor having two thresholds, up_threshold and
    down_threshold to decide whether to increase the frequency or
    scale it down respectively. The sampling interval is in the
    order of milliseconds and should be chosen by host drivers that
    enable MMC_CAP2_CLK_SCALE capability to take advantage of clock
    scaling. The sampling interval mainly depends on the the clock
    switching delays and hence a host driver decision. If sampling
    interval is too low frequent switching of frequencies can lead
    to high power consumption and if sampling interval is too high,
    the clock scaling logic would take long time to realize that the
    underlying hardware (controller and card) is busy and scale up
    the clocks.
    
    Change-Id: I22a5054beec41b0b66b3bf030ddfcf284de448b3
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -144,6 +144,8 @@ struct mmc_host_ops {
 	int	(*select_drive_strength)(unsigned int max_dtr, int host_drv, int card_drv);
 	void	(*hw_reset)(struct mmc_host *host);
 	void	(*card_event)(struct mmc_host *host);
+	unsigned long (*get_max_frequency)(struct mmc_host *host);
+	unsigned long (*get_min_frequency)(struct mmc_host *host);
 };
 
 struct mmc_card;
@@ -304,6 +306,8 @@ struct mmc_host {
 				(1 << 18))	/* Allow command queuing */
 #define MMC_CAP2_PACKED_WR_CONTROL (1 << 19)	/* Allow write packing control */
 
+#define MMC_CAP2_CLK_SCALE	(1 << 20)	/* Allow dynamic clk scaling */
+
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 
 	unsigned int		dev_drv_str;	/* device driver strength */
@@ -442,6 +446,19 @@ struct mmc_host {
 
 	bool perf_enable;
 #endif
+	struct {
+		unsigned long	busy_time_us;
+		unsigned long	window_time;
+		unsigned long	curr_freq;
+		unsigned long	polling_delay_ms;
+		unsigned int	up_threshold;
+		unsigned int	down_threshold;
+		ktime_t		start_busy;
+		bool		enable;
+		bool		initialized;
+		bool		in_progress;
+		struct delayed_work work;
+	} clk_scaling;
 	unsigned long		private[0] ____cacheline_aligned;
 };
 
