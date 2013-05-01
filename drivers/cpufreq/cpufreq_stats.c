de52c6de678293c70f0a39d2e8b9a0efac7552dc
Archana Sathyakumar <asathyak@codeaurora.org>
2013-05-01 15:26:36 -0600

    cpufreq: Resolve CPUFREQ_NOTIFY issue
    When the cpufreq_stat receives the frequency policy notification,
    it checks if the frequency table and stats table are present for the cpu.
    If both are present, it returns with -EBUSY error to the notification.
    Due to this, none of the other registered modules receive this
    notification.
    
    If the stat table and sysfs nodes are already created, return 0 instead
    of returning EBUSY error.
    
    Signed-off-by: Archana Sathyakumar <asathyak@codeaurora.org>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -642,7 +642,8 @@ static int cpufreq_stats_create_table(struct cpufreq_policy *policy,
 	struct cpufreq_stats *prev_stat = per_cpu(prev_cpufreq_stats_table, cpu);
 
 	if (per_cpu(cpufreq_stats_table, cpu))
-		return -EBUSY;
+		return 0;
+
 	stat = kzalloc(sizeof(struct cpufreq_stats), GFP_KERNEL);
 	if ((stat) == NULL)
 		return -ENOMEM;
