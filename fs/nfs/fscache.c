c91e869c2d6c3fe6088cc6999c9e3a5527df3108
James Christopher Adduono <jc@adduono.com>
2017-01-08 08:00:47 -0500

    Fix derpy coding

--- fs/nfs/fscache.c
+++ fs/nfs/fscache.c
@@ -284,8 +284,7 @@ static inline void nfs_fscache_inode_unlock(struct inode *inode)
 {
 	struct nfs_inode *nfsi = NFS_I(inode);
 
-	smp_mb__before_atomic();
-	clear_bit(NFS_INO_FSCACHE_LOCK, &nfsi->flags);
+	clear_bit_unlock(NFS_INO_FSCACHE_LOCK, &nfsi->flags);
 	smp_mb__after_atomic();
 	wake_up_bit(&nfsi->flags, NFS_INO_FSCACHE_LOCK);
 }
