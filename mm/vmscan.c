alexax66/kernel_samsung_a3xelte

mm: vmscan: shrink_slab: rename max_pass -> freeable
The name `max_pass' is misleading, because this variable actually keeps
the estimate number of freeable objects, not the maximal number of
objects we can scan in this pass, which can be twice that.  Rename it to
reflect its actual meaning.

Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
Acked-by: David Rientjes <rientjes@google.com>
Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

Original commit: https://github.com/hizukiayaka/linux-kernel/commit/d5bc5fd3fcb7b8dfb431694a8c8052466504c10c

mm/vmscan.c
@@ -257,7 +257,7 @@ shrink_slab_node(struct shrink_control *shrinkctl, struct shrinker *shrinker,
         unsigned long freed = 0;
         unsigned long long delta;
         long total_scan;
-        long max_pass;
+        long freeable;
         long nr;
         long new_nr;
         int nid = shrinkctl->nid;
@@ -269,10 +269,10 @@ shrink_slab_node(struct shrink_control *shrinkctl, struct shrinker *shrinker,
                 min_cache_size = 0;
 
         if (shrinker->count_objects)
-                max_pass = shrinker->count_objects(shrinker, shrinkctl);
+                freeable = shrinker->count_objects(shrinker, shrinkctl);
         else
-                max_pass = do_shrinker_shrink(shrinker, shrinkctl, 0);
-        if (max_pass == 0)
+                freeable = do_shrinker_shrink(shrinker, shrinkctl, 0);
+        if (freeable == 0)
                 return 0;
 
         /*
@@ -284,14 +284,14 @@ shrink_slab_node(struct shrink_control *shrinkctl, struct shrinker *shrinker,
 
         total_scan = nr;
         delta = (4 * nr_pages_scanned) / shrinker->seeks;
-        delta *= max_pass;
+        delta *= freeable;
         do_div(delta, lru_pages + 1);
         total_scan += delta;
         if (total_scan < 0) {
                 printk(KERN_ERR
                 "shrink_slab: %pF negative objects to delete nr=%ld\n",
                        shrinker->shrink, total_scan);
-                total_scan = max_pass;
+                total_scan = freeable;
         }
 
         /*
@@ -300,26 +300,26 @@ shrink_slab_node(struct shrink_control *shrinkctl, struct shrinker *shrinker,
          * shrinkers to return -1 all the time. This results in a large
          * nr being built up so when a shrink that can do some work
          * comes along it empties the entire cache due to nr >>>
-         * max_pass.  This is bad for sustaining a working set in
+         * freeable.  This is bad for sustaining a working set in
          * memory.
          *
          * Hence only allow the shrinker to scan the entire cache when
          * a large delta change is calculated directly.
          */
-        if (delta < max_pass / 4)
-                total_scan = min(total_scan, max_pass / 2);
+        if (delta < freeable / 4)
+                total_scan = min(total_scan, freeable / 2);
 
         /*
          * Avoid risking looping forever due to too large nr value:
          * never try to free more than twice the estimate number of
          * freeable entries.
          */
-        if (total_scan > max_pass * 2)
-                total_scan = max_pass * 2;
+        if (total_scan > freeable * 2)
+                total_scan = freeable * 2;
 
         trace_mm_shrink_slab_start(shrinker, shrinkctl, nr,
                                 nr_pages_scanned, lru_pages,
-                                max_pass, delta, total_scan);
+                                freeable, delta, total_scan);
 
         while (total_scan > min_cache_size) {
 

Exynos7580/android_kernel_samsung_exynos7580-common

Conflicts:
	mm/vmscan.c

mm/vmscan.c
@@ -270,15 +270,15 @@ unsigned long shrink_slab(struct shrink_control *shrink,
 	list_for_each_entry(shrinker, &shrinker_list, list) {
 		unsigned long long delta;
 		long total_scan, pages_got;
-		long max_pass;
+		long freeable;
 		int shrink_ret = 0;
 		long nr;
 		long new_nr;
 		long batch_size = shrinker->batch ? shrinker->batch
 						  : SHRINK_BATCH;
 
-		max_pass = do_shrinker_shrink(shrinker, shrink, 0);
-		if (max_pass <= 0)
+		freeable = do_shrinker_shrink(shrinker, shrink, 0);
+		if (freeable <= 0)
 			continue;
 
 		/*
@@ -290,14 +290,14 @@ unsigned long shrink_slab(struct shrink_control *shrink,
 
 		total_scan = nr;
 		delta = (4 * nr_pages_scanned) / shrinker->seeks;
-		delta *= max_pass;
+		delta *= freeable;
 		do_div(delta, lru_pages + 1);
 		total_scan += delta;
 		if (total_scan < 0) {
 			printk(KERN_ERR "shrink_slab: %pF negative objects to "
 			       "delete nr=%ld\n",
 			       shrinker->shrink, total_scan);
-			total_scan = max_pass;
+			total_scan = freeable;
 		}
 
 		/*
@@ -306,26 +306,26 @@ unsigned long shrink_slab(struct shrink_control *shrink,
 		 * shrinkers to return -1 all the time. This results in a large
 		 * nr being built up so when a shrink that can do some work
 		 * comes along it empties the entire cache due to nr >>>
-		 * max_pass.  This is bad for sustaining a working set in
+		 * freeable.  This is bad for sustaining a working set in
 		 * memory.
 		 *
 		 * Hence only allow the shrinker to scan the entire cache when
 		 * a large delta change is calculated directly.
 		 */
-		if (delta < max_pass / 4)
-			total_scan = min(total_scan, max_pass / 2);
+		if (delta < freeable / 4)
+			total_scan = min(total_scan, freeable / 2);
 
 		/*
 		 * Avoid risking looping forever due to too large nr value:
 		 * never try to free more than twice the estimate number of
 		 * freeable entries.
 		 */
-		if (total_scan > max_pass * 2)
-			total_scan = max_pass * 2;
+		if (total_scan > freeable * 2)
+			total_scan = freeable * 2;
 
 		trace_mm_shrink_slab_start(shrinker, shrink, nr,
 					nr_pages_scanned, lru_pages,
-					max_pass, delta, total_scan);
+					freeable, delta, total_scan);
 
 		while (total_scan >= batch_size) {
 			int nr_before;