f4a1f1ccc812f0361d211e4377a379d05c777484
Alexander Alexeev <alexax66@gmail.com>
2018-11-27 00:25:42 +0300

    build_kernel: rework for locally building

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,40 +1,16 @@
 #!/bin/bash
 
-DTBH_PLATFORM_CODE=0x50a6
-DTBH_SUBTYPE_CODE=0x217584da
-
 export ARCH=arm64
-
-case "$1" in
-	a3)
-	    VARIANT="a3xelte"
-	    ;;
-
-	a5)
-	    VARIANT="a5xelte"
-	    ;;
-
-	j7)
-	    VARIANT="j7elte"
-	    ;;
-
-	s5)
-	    VARIANT="s5neolte"
-	    ;;
-
-	*)
-	    VARIANT="a3xelte"
-esac
-
 
 if [ ! -d $(pwd)/output ];
     then
         mkdir $(pwd)/output;
     fi
 
-make -C $(pwd) O=output ARCH=arm64 "lineageos_"$VARIANT"_defconfig"
+make -C $(pwd) O=output ARCH=arm64 "los_a3xelte_defconfig"
 make -j7 -C $(pwd) O=output ARCH=arm64
 
 cp output/arch/arm64/boot/Image  output/arch/arm64/boot/boot.img-kernel
+cp output/arch/arm64/boot/dtb.img  output/arch/arm64/boot/boot.img-dt
 
 exit
