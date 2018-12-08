61f43df3c713d6605dc108b41049db5d9956c499
Alexander Alexeev <alexax66@gmail.com>
2018-12-08 23:00:30 +0300

    Revert "arm64: thread_info remove stale items"
    This reverts commit 3fd34ad1f0b39f6c783d9b30b3ef0fdf70c62b6d.

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -41,6 +41,7 @@ typedef unsigned long mm_segment_t;
 
 /*
  * low level task data that entry.S needs immediate access to.
+ * __switch_to() assumes cpu_context follows immediately after cpu_domain.
  */
 struct thread_info {
 	unsigned long		flags;		/* low level flags */
@@ -64,6 +65,7 @@ struct thread_info {
 	},								\
 }
 
+#define init_thread_info	(init_thread_union.thread_info)
 #define init_stack		(init_thread_union.stack)
 
 /*
