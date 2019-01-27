c0af57a3ff26a46aa1dde8698ab3ed4e4bc9f3e4
Alexander Alexeev <alexax66@gmail.com>
2019-01-27 21:14:40 +0300

    build_kernel: add variant for LineageOS 16.0

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,13 +2,29 @@
 
 export ARCH=arm64
 
+THREAD=-j$(bc <<< $(grep -c ^processor /proc/cpuinfo)+2)
+
+case "$1" in
+        15)
+            VARIANT="los"
+            ;;
+
+        16)
+            VARIANT="los16"
+            ;;
+
+        *)
+            VARIANT="los"
+esac
+
 if [ ! -d $(pwd)/output ];
     then
         mkdir $(pwd)/output;
     fi
 
-make -C $(pwd) O=output ARCH=arm64 "los_a3xelte_defconfig"
-make -j7 -C $(pwd) O=output ARCH=arm64
+make -C $(pwd) O=output ARCH=arm64 ""$VARIANT"_a3xelte_defconfig"
+
+make $THREAD -C $(pwd) O=output ARCH=arm64
 
 cp output/arch/arm64/boot/Image  output/arch/arm64/boot/boot.img-kernel
 cp output/arch/arm64/boot/dtb.img  output/arch/arm64/boot/boot.img-dt
