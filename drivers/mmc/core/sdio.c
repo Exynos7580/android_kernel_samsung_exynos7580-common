LineageOS/android_kernel_samsung_universal7580

merge G903WVLU1CQI1 changes
Change-Id: I29ca6a879f6b97d6a0318a103ba5e4de370c66c2

drivers/mmc/core/sdio.c
@@ -1230,6 +1230,15 @@ int mmc_attach_sdio(struct mmc_host *host)
 			goto remove_added;
 	}
 
+#if defined(CONFIG_BCM43455) || defined(CONFIG_BCM43455_MODULE)|| \
+    defined(CONFIG_BCM4343) || defined(CONFIG_BCM4343_MODULE) || \
+    defined(CONFIG_BCM43454) || defined(CONFIG_BCM43454_MODULE)
+	if(!strcmp("mmc1", mmc_hostname(host))) {
+		printk("%s, Set Nonremovable flag\n",mmc_hostname(host));
+		host->caps |= MMC_CAP_NONREMOVABLE;
+	}
+#endif /* CONFIG_BCM43455 */
+
 	mmc_claim_host(host);
 	return 0;
 

Exynos7580/android_kernel_samsung_exynos7580-common

drivers/mmc/core/sdio.c
@@ -1230,6 +1230,15 @@ int mmc_attach_sdio(struct mmc_host *host)
 			goto remove_added;
 	}
 
+#if defined(CONFIG_BCM4343) || defined(CONFIG_BCM4343_MODULE) || \
+    defined(CONFIG_BCM43455) || defined(CONFIG_BCM43455_MODULE)|| \
+    defined(CONFIG_BCM43454) || defined(CONFIG_BCM43454_MODULE)
+	if(!strcmp("mmc1", mmc_hostname(host))) {
+		printk("%s, Set Nonremovable flag\n",mmc_hostname(host));
+		host->caps |= MMC_CAP_NONREMOVABLE;
+	}
+#endif /* CONFIG_BCM43455 */
+
 	mmc_claim_host(host);
 	return 0;
 