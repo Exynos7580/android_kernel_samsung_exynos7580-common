7333faf57fd8f792edc8385da16179dc50dc3b74
Alexander Alexeev <alexax66@gmail.com>
2019-04-13 00:36:04 +0300

    Revert "mmc: Schedule mmcqd on idle CPU"
    This reverts commit c2acb84eb0ba3a9425bc037ac91e43f631d41647.

--- drivers/mmc/card/queue.c
+++ drivers/mmc/card/queue.c
@@ -105,7 +105,6 @@ static int mmc_queue_thread(void *d)
 	sched_setscheduler(current, SCHED_FIFO, &scheduler_params);
 
 	current->flags |= PF_MEMALLOC;
-	set_wake_up_idle(true);
 
 #if defined(CONFIG_SOC_EXYNOS5430) || defined(CONFIG_SOC_EXYNOS5433)
 	set_cpus_allowed_ptr(current, cpu_coregroup_mask(0));
